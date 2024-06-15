1b844a3f6d4780cdb7e456d1d765026295900ec8
Tijn Van de Velde <70897975+TijnVdVelde@users.noreply.github.com>
2024-06-15 17:03:19 +0200

    update config.js

--- src/util/config.js
+++ src/util/config.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const secretkey = process.env.SECRETKEY || 'DitIsEenGeheim';
 
 const config = {
     secretkey: secretkey,
-    dbHost: 'localhost',
-    dbUser: 'root',
-    dbPassword: '',
-    dbPort: 3307,
-    dbDatabase: 'share_a_meal'
+    dbHost: process.env.DB_HOST || 'localhost',
+    dbUser: process.env.DB_USER || 'root',
+    dbPassword: process.env.DB_PASSWORD || '',
+    dbPort: process.env.DB_PORT || 3307,
+    dbDatabase: process.env.DB_DATABASE || 'share_a_meal'
 };
 
 module.exports = config;
