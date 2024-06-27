f62ae7245dced584beeadae531f5bcac83e5fc97
Tijn Van de Velde <70897975+TijnVdVelde@users.noreply.github.com>
2024-06-27 09:59:46 +0200

    config.js was interfering

--- src/util/config.js
+++ src/util/config.js
@@ -1,17 +1,14 @@
-require('dotenv').config(); // Importeer de dotenv module en laad de environment variabelen uit een .env bestand.
-
-const dbUrl = process.env.DATABASE_URL || 'mysql://root:password@localhost:3307/share_a_meal'; 
-// Definieer de database-URL, gebruik de waarde uit de environment variabelen of een standaardwaarde als fallback.
+require('dotenv').config() // Import and load environment variables from .env file
 
 const config = {
-    secretkey: process.env.SECRETKEY || 'DitIsEenGeheim', // De geheime sleutel voor het signeren van JWT's, haal uit de environment variabelen of gebruik een standaardwaarde.
-    dbHost: process.env.MYSQLHOST || 'localhost', // De hostnaam van de database, haal uit de environment variabelen of gebruik een standaardwaarde.
-    dbUser: process.env.MYSQLUSER || 'root', // De gebruikersnaam voor de database, haal uit de environment variabelen of gebruik een standaardwaarde.
-    dbPassword: process.env.MYSQLPASSWORD || '', // Het wachtwoord voor de databasegebruiker, haal uit de environment variabelen of gebruik een standaardwaarde.
-    dbPort: process.env.MYSQLPORT || 3307, // De poort waarop de database draait, haal uit de environment variabelen of gebruik een standaardwaarde.
-    dbDatabase: process.env.MYSQLDATABASE || 'share_a_meal' // De naam van de database, haal uit de environment variabelen of gebruik een standaardwaarde.
-};
+    secretkey: process.env.SECRETKEY || 'DitIsEenGeheim', // Secret key for signing JWTs
+    dbHost: process.env.DB_HOST || 'localhost', // Database host
+    dbUser: process.env.DB_USER || 'root', // Database user
+    dbPassword: process.env.DB_PASSWORD || '', // Database password
+    dbPort: process.env.DB_PORT || 3306, // Database port
+    dbDatabase: process.env.DB_DATABASE || 'share_a_meal' // Database name
+}
 
-console.log('Database Config:', config); // Voeg deze regel toe om de configuratie-instellingen naar de console te printen.
+console.log('Database Config:', config) // Log the configuration settings
 
-module.exports = config; // Exporteer de configuratie-instellingen zodat deze gebruikt kunnen worden in andere delen van de applicatie.
+module.exports = config // Export the configuration
